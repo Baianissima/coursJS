5e88138b78a7f9a9ce792257acdbc47267ba55b5
Vanusa Santos <vbrasil.tradservices@gmail.com>
2021-09-24 16:57:32 +0200

    Intro JS / Boostrap / link pour boostrap

--- 01_introduction/introduction.js
+++ 01_introduction/introduction.js
@@ -6,18 +6,18 @@
 Avec barre oblique + étoile pour creer ce commentaire
 */
 
-let bonjour = document.getElementById('b1');
-let ajouter = document.getElementById('b2');
+// let bonjour = document.getElementById('b1');
+// let ajouter = document.getElementById('b2');
 
-bonjour.addEventListener('click', popup);
-ajouter.addEventListener('click', ajout);
+// bonjour.addEventListener('click', popup);
+// ajouter.addEventListener('click', ajout);
 
-function popup(){
-    alert('Hé, Patrick, vous avez un chargeur ?');
-}
+// function popup(){
+//     alert('Hé, Patrick, vous avez un chargeur ?');
+// }
 
-function ajout(){
-    let para = document.createElement('p');
-    para.textContent = 'Paragraphe ajouté';
-    document.body.appendChild(para);
-}
+// function ajout(){
+//     let para = document.createElement('p');
+//     para.textContent = 'Paragraphe ajouté';
+//     document.body.appendChild(para);
+// }
