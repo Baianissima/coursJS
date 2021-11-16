21c37fa5713275a5f23dc9febf896e1605264f25
Vanusa Santos <vbrasil.tradservices@gmail.com>
2021-11-16 16:58:21 +0100

    maj

--- santosEXO16/santos_exo16.js
+++ santosEXO16/santos_exo16.js
@@ -1,21 +1,38 @@
 console.log ('Boa tarde !');
 
-// Exo 16 : faire une boucle while
+// Exo 16 : faire une boucle while : vaut 5 points
 
+console.log ('---- EXO 16 : une boucle while ----');
 
-// boucle WHILE
+// CORRECTION DE PATRICK :
 
+let disMoi = '';
+let i = 1;
 
+// Ici on lit "Tant que le prompt ne contient pas oui ET ne contient pas non, continue à éxecuter le prompt" :
 
-let jeu = '';
+while ((disMoi !== 'oui') && (disMoi !== 'non')) {
+    disMoi = prompt('Maintenant, nous allons jouer au jeu "Oui ni Non !" Ecris ce que tu veux ici !');
+    i++; // une boucle a besoin de nombre de tours : il faut mettre i++
 
-while ((jeu !== 'oui') && (jeu !== 'non')) {
-    taReponse = prompt('Maintenant, nous allons jouer au jeu "Oui ni Non !" Ecris ce que tu veux ici !');
-
-    document.getElementById('p1').innerHTML += 'Tu as tapé ' + taReponse + ', donc tu as perdu !';
-
+    document.getElementById('p1').classList.add('stabilo');
+    document.getElementById('p1').innerHTML = 'Vous avez perdu !';
 }
-console.info(taReponse);
+console.info(disMoi);
+console.log ('---- boucle terminée ----');
+
+
+
+//Ma version rendue:
+// let jeu = '';
+
+// while ((jeu !== 'oui') && (jeu !== 'non')) {
+//     taReponse = prompt('Maintenant, nous allons jouer au jeu "Oui ni Non !" Ecris ce que tu veux ici !');
+
+//     document.getElementById('p1').innerHTML += 'Tu as tapé ' + taReponse + ', donc tu as perdu !';
+
+// }
+// console.info(taReponse);
 
 
 
